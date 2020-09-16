33a77f66adee9497de7124fbd0c287c4a729cf9d
Anthony Ting <anthonydting@gmail.com>
2020-09-16 02:02:18 -0700

    Optimize sportsday command

--- sportsday/sportsday.py
+++ sportsday/sportsday.py
@@ -6,18 +6,24 @@ import random
 class SportsDay(commands.Cog):
     """Sports Day"""
 
+    def __init__(self):
+        self.sportsdaybj = None
+        self.sportsdayvk = None
+
     @commands.command(pass_context=True)
     @commands.guild_only()
     async def sportsday(self, ctx: commands.Context):
         """Sport Day"""
         await ctx.trigger_typing()
-
-        sportsdaybj = discord.utils.get(ctx.guild.emojis, name='sportsdaybj')
-        sportsdayvr = discord.utils.get(ctx.guild.emojis, name='sportsdayvik')
+        
+        if (not self.sportsdaybj):
+            self.sportsdaybj = discord.utils.get(ctx.guild.emojis, name='sportsdaybj')
+        if (not self.sportsdayvk):
+            self.sportsdayvk = discord.utils.get(ctx.guild.emojis, name='sportsdayvik')
 
         isbj = random.randint(0, 100)
 
-        sportsday = sportsdaybj if (isbj < 60) else sportsdayvr
+        sportsday = self.sportsdaybj if (isbj < 60) else self.sportsdayvk
 
         rows = random.randint(1, 6)
         cols = random.randint(1, 6)
