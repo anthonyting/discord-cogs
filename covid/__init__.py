fec421da53f62b277a95d12a319b17606dea8f8b
Anthony Ting <anthonydting@gmail.com>
2020-11-26 00:23:47 -0800

    initial commit for scraper for covid report

--- covid/__init__.py
+++ covid/__init__.py
@@ -1,4 +1,6 @@
 from .covid import Covid
+from .report import Report
 
 def setup(bot):
     bot.add_cog(Covid())
+    bot.add_cog(Report())
---
+++ covid/report.py
@@ -0,0 +1,73 @@
+from redbot.core import commands
+import bs4
+import urllib.request
+import re
+from bs4.element import PageElement, Tag
+import pyteaser
+from datetime import datetime
+import time
+
+class Report(commands.Cog):
+    """Start report"""
+
+    def __init__(self):
+        self.running = False
+        self.today = None
+        self.foundToday = False
+
+    @commands.command(pass_context=True)
+    @commands.guild_only()
+    @commands.is_owner()
+    async def report(self, ctx: commands.Context):
+        if (self.running):
+            await ctx.send("Already reporting")
+            return
+
+        self.running = True
+
+        await ctx.send("Reporting in this channel")
+        while(True):
+            if (self.today == datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)):
+                time.sleep(3600)
+                continue
+            else:
+                self.today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+            directoryUrl = urllib.request.urlopen(
+                'https://news.gov.bc.ca/ministries/health')
+
+            directory = bs4.BeautifulSoup(directoryUrl, features="html.parser")
+
+            todayString = datetime.now().strftime(r"%B %#d, %Y")
+            dates = directory.find_all(
+                name='span', attrs={"class": "item-date"}, text=re.compile(f"{todayString}.*"))
+            statement: PageElement = None
+            for date in dates:
+                located: PageElement = date.parent.parent.find(
+                    name='a', href=True, text=re.compile(f"Joint statement.*COVID-19.*"))
+                if (located):
+                    statement = located
+                    break
+
+            if (statement and statement['href']):
+                jointStatementUrl = urllib.request.urlopen(statement['href'])
+                jointStatement = bs4.BeautifulSoup(
+                    jointStatementUrl, features="html.parser")
+                textElement: Tag = jointStatement.select_one(
+                    selector=".story-expander article")
+                if (textElement):
+                    text: str = textElement.getText()
+                    intro: str = "Dr. Bonnie Henry, B.C.’s provincial health officer, and Adrian Dix, Minister of Health, have issued the following joint statement regarding updates on the novel coronavirus (COVID-19) response in British Columbia:"
+                    concatenateStart: int = text.find(intro)
+                    if (concatenateStart > -1):
+                        concatenateStart += len(intro)
+                        concatenateEnd: int = text.rfind("Learn More:")
+                        if (concatenateStart > -1 and concatenateEnd > -1):
+                            text = text[concatenateStart:concatenateEnd]
+                            text = re.sub(re.compile('”'), '', re.sub(
+                                re.compile('“'), ' ', text)).strip()
+                            self.foundToday = True
+                            await ctx.send(" ".join(pyteaser.Summarize(
+                                statement.getText(), text)))
+            else:
+                time.sleep(3600)  # one hour
