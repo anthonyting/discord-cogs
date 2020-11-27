93bf51391a35bdf50e3b3907b443d558e5b5a020
Anthony Ting <anthonydting@gmail.com>
2020-11-26 23:03:37 -0800

    fix exceptions not getting printed, and remove unused self.today

--- covid/report.py
+++ covid/report.py
@@ -14,7 +14,6 @@ class Report(commands.Cog):
     """Start report"""
 
     def __init__(self):
-        self.today = None
         self.foundToday = False
         self.firstRun = True
         self.task: Task = None
@@ -35,8 +34,8 @@ class Report(commands.Cog):
             await ctx.send("Task not started")
 
         self.foundToday = False
-        self.firstRun = False
-        self.today = None
+        self.firstRun = True
+        self.count = 0
         self.stopTask()
         await ctx.send("Task reset success")
 
@@ -50,6 +49,8 @@ class Report(commands.Cog):
 
         await ctx.send("Starting task in this channel")
         self.task = asyncio.create_task(self.run(ctx))
+        await self.task
+        print(self.task.exception())
 
     async def run(self, ctx: commands.Context):
         while(self.task is not None):
@@ -59,9 +60,9 @@ class Report(commands.Cog):
             await asyncio.sleep(10)
             now: datetime = datetime.now()
             noon: datetime = now.replace(
-                hours=12, minute=0, second=0, microsecond=0)
+                hour=12, minute=0, second=0, microsecond=0)
             night: datetime = now.replace(
-                hours=22, minute=0, second=0, microsecond=0)
+                hour=22, minute=0, second=0, microsecond=0)
             if (now > noon):
                 if (self.foundToday or now > night):
                     # wait for tomorrow if we have today or if it's too late
@@ -69,56 +70,55 @@ class Report(commands.Cog):
                     timeUntilNextCheck: float = (
                         noonTomorrow - now).total_seconds()
                 else: # otherwise check every 15 minutes
-                    timeUntilNextCheck: float = 750.0  # 15 minutes
+                    timeUntilNextCheck: float = 750  # 15 minutes
             else:
                 timeUntilNextCheck: float = (noon - now).total_seconds()
                 self.foundToday = False
             if (not self.firstRun):
                 await asyncio.sleep(timeUntilNextCheck)
             self.firstRun = False
-            if (self.today != datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)):
-                with urlopen('https://news.gov.bc.ca/ministries/health') as directoryUrl:
-                    directory = bs4.BeautifulSoup(
-                        directoryUrl, features="html.parser")
+            with urlopen('https://news.gov.bc.ca/ministries/health') as directoryUrl:
+                directory = bs4.BeautifulSoup(
+                    directoryUrl, features="html.parser")
 
-                    todayString = datetime.now().strftime(r"%B %#d, %Y")
-                    dates = directory.find_all(
-                        name='span', attrs={"class": "item-date"}, text=re.compile(f"{todayString}.*"))
-                    statement: PageElement = None
-                    for date in dates:
-                        located: PageElement = date.parent.parent.find(
-                            name='a', href=True, text=re.compile(f"Joint statement.*COVID-19.*"))
-                        if (located):
-                            statement = located
-                            break
+                todayString = datetime.now().strftime(r"%B %#d, %Y")
+                dates = directory.find_all(
+                    name='span', attrs={"class": "item-date"}, text=re.compile(f"{todayString}.*"))
+                statement: PageElement = None
+                for date in dates:
+                    located: PageElement = date.parent.parent.find(
+                        name='a', href=True, text=re.compile(f"Joint statement.*COVID-19.*"))
+                    if (located):
+                        statement = located
+                        break
 
-                    if (statement and statement['href']):
-                        with urlopen(statement['href']) as jointStatementUrl:
-                            jointStatement = bs4.BeautifulSoup(
-                                jointStatementUrl, features="html.parser")
-                            textElement: Tag = jointStatement.select_one(
-                                selector=".story-expander article")
-                            if (textElement):
-                                text: str = textElement.getText()
-                                intro: str = "Dr. Bonnie Henry, B.C.’s provincial health officer, and Adrian Dix, Minister of Health, have issued the following joint statement regarding updates on the novel coronavirus (COVID-19) response in British Columbia:"
-                                concatenateStart: int = text.find(intro)
-                                if (concatenateStart > -1):
-                                    concatenateStart += len(intro)
-                                    concatenateEnd: int = text.rfind(
-                                        "Learn More:")
-                                    if (concatenateStart > -1 and concatenateEnd > -1):
-                                        text = text[concatenateStart:concatenateEnd]
-                                        text = re.sub(re.compile('”'), '', re.sub(
-                                            re.compile('“'), ' ', text)).strip()
-                                        self.foundToday = True
-                                        embed = discord.Embed(
-                                            title="BC COVID-19 Joint Statement",
-                                            description=" ".join(
-                                                pyteaser.Summarize(statement.getText(), text)),
-                                            colour=discord.Colour.blue()
-                                        )
-                                        embed.set_author(
-                                            name=f"Source", url=statement['href'], icon_url=r'https://cdn.discordapp.com/attachments/360564259316301836/747043112043544617/BCGov_-_Horizontal_AGOL_Logo_-_White_-_Sun.png')
+                if (statement and statement['href']):
+                    with urlopen(statement['href']) as jointStatementUrl:
+                        jointStatement = bs4.BeautifulSoup(
+                            jointStatementUrl, features="html.parser")
+                        textElement: Tag = jointStatement.select_one(
+                            selector=".story-expander article")
+                        if (textElement):
+                            text: str = textElement.getText()
+                            intro: str = "Dr. Bonnie Henry, B.C.’s provincial health officer, and Adrian Dix, Minister of Health, have issued the following joint statement regarding updates on the novel coronavirus (COVID-19) response in British Columbia:"
+                            concatenateStart: int = text.find(intro)
+                            if (concatenateStart > -1):
+                                concatenateStart += len(intro)
+                                concatenateEnd: int = text.rfind(
+                                    "Learn More:")
+                                if (concatenateStart > -1 and concatenateEnd > -1):
+                                    text = text[concatenateStart:concatenateEnd]
+                                    text = re.sub(re.compile('”'), '', re.sub(
+                                        re.compile('“'), ' ', text)).strip()
+                                    self.foundToday = True
+                                    embed = discord.Embed(
+                                        title="BC COVID-19 Joint Statement",
+                                        description=" ".join(
+                                            pyteaser.Summarize(statement.getText(), text)),
+                                        colour=discord.Colour.blue()
+                                    )
+                                    embed.set_author(
+                                        name=f"Source", url=statement['href'], icon_url=r'https://cdn.discordapp.com/attachments/360564259316301836/747043112043544617/BCGov_-_Horizontal_AGOL_Logo_-_White_-_Sun.png')
 
-                                        self.count = 0
-                                        await ctx.send(embed=embed)
+                                    self.count = 0
+                                    await ctx.send(embed=embed)
