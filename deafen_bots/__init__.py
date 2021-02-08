399bb78a8c0d048935960981f316b9fe097d5f45
Anthony Ting <anthonydting@gmail.com>
2021-02-08 01:38:02 -0800

    prevent bots from being undeafened

---
+++ deafen_bots/__init__.py
@@ -0,0 +1,18 @@
+import discord
+from redbot.core import commands
+
+class Deafen(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        if (not after.deaf and member.bot):
+            await member.edit(reason="Bots don't need to hear", deafen=True)
+
+def setup(bot):
+    bot.add_cog(Deafen(bot))
+
+
+def teardown(bot):
+    pass
